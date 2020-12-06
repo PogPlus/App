a4af4a868b0d9d1bb98704178d8d5c3ba74a162e
Jack Merrill <me@jackmerrill.com>
2020-12-06 13:42:37 -0600

    fixed bugs, added features idk man

--- pages/upload.js
+++ pages/upload.js
@@ -1,17 +1,66 @@
 import HeaderNav from "../components/Navbar";
-import { Spinner } from 'react-bootstrap'
-import { useSession } from 'next-auth'
+import { Form, Spinner, Container } from 'react-bootstrap'
+import { useSession, signIn } from 'next-auth/client'
+import { useState } from "react";
 
-export default function Upload() {
+import Axios from 'axios'
+
+// const preventDefault = f => e => {
+//     e.preventDefault()
+//     f(e)
+// }
+
+// const toBase64 = file => new Promise((resolve, reject) => {
+//     const reader = new FileReader();
+//     reader.readAsDataURL(file);
+//     reader.onload = () => resolve(reader.result);
+//     reader.onerror = error => reject(error);
+// });
+
+export default async function Upload() {
     const [session, loading] = useSession()
+
+    const [title, setTitle] = useState("")
+
+    const [file, setFile] = useState("")
+
+    if (!session && !loading) await signIn("discord", { callbackUrl: "/profile" })
+
+    console.log("signin")
+
+    const handleSubmit = () => {
+        let image
+        toBase64(file).then(result => {
+            image = result
+        })
+        Axios.post("http://localhost:4000/api/post", {
+            "title": title,
+            "image": image,
+            "user": session
+        })
+    }
+
+    const handleChange = setValue => e => setValue(e.target.value)
+
     return (
         <>
-            {loading && <>
-                <Spinner animation="border" role="status">
-                    <span className="sr-only">Loading...</span>
-                </Spinner>
-            </>}
+            {console.log("header")}
             <HeaderNav />
+            <Container>
+                {console.log("container")}
+                    <Form onSubmit={handleSubmit}>
+                                <Form.Label>Title</Form.Label>
+                                <Form.Control type="string" placeholder="Enter Title" onChange={handleChange(setTitle)} value={title} />
+                                <Form.File 
+                                id="image"
+                                label="Meme to Upload"
+                                custom
+                                value={file}
+                                onChange={handleChange(setFile)}
+                            />
+                            {console.log("form")}
+                    </Form>
+                </Container>
         </>
     )
 }
