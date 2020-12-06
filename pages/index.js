7462c31f7822c1e67d58aeb591b6754d9c93ecdc
Saketh <sakethmadhu7@gmail.com>
2020-12-06 12:46:55 -0700

    changes

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,11 @@ export default function Home() {
     <>
       <ToastContainer />
       <HeaderNav />
-      <Container style={{ backgroundColor: "#201f22", height: "100%", display: 'flex', justifyContent: 'center' }}>
+      <Container className="container" style={{ backgroundColor: "#201f22", height: "100%", display: 'flex', justifyContent: 'center' }}>
         <Post title="pog" _id="yesrandomlettershere" image="https://cdn.discordapp.com/attachments/715332913419255868/784990721677328394/FullSizeRender.png" user={{ _id: "HDkjSHdkJ", username: "bruh moment" }}/>
+
         <Post title="pog" _id="yesrandomlettershere" image="https://cdn.discordapp.com/attachments/715332913419255868/784990721677328394/FullSizeRender.png" user={{ _id: "HDkjSHdkJ", username: "bruh moment" }}/>
+
         <Post title="pog" _id="yesrandomlettershere" image="https://cdn.discordapp.com/attachments/715332913419255868/784990721677328394/FullSizeRender.png" user={{ _id: "HDkjSHdkJ", username: "bruh moment" }}/>
       </Container>
     </>
